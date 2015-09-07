56c8b0bb11a45a8f823cddc2b0c0b4fa8fa5cf6f
Reid Paape <wrpaape@gmail.com>
2015-09-07 11:49:06 -0500

    returning to css scaling :(

--- draw_app/app/assets/javascripts/components/index.js.jsx
+++ draw_app/app/assets/javascripts/components/index.js.jsx
@@ -2,22 +2,38 @@
 'use strict';
 
 var Index = React.createClass({
+  getInitialState: function() {
+    return this.getDims();
+  },
   componentDidMount: function() {
+    window.addEventListener('resize', this.refreshDims);
     this.draw();
   },
   render: function() {
-    return(
-      <canvas id='canvas'>
-      </canvas>
-    );
+    // var style = {
+    //   width: this.state.width + 'px',
+    //   height: this.state.height + 'px'
+    // };
+    var style = {
+      width: '200px',
+      height: '200px'
+    }
+
+    return <canvas style={ style } />;
+  },
+  getDims: function() {
+    var width = window.innerWidth / 2;
+
+    return({
+      width: width,
+      height: width / 2
+    });
+  },
+  refreshDims: function() {
+    this.setState(this.getDims());
   },
   draw: function() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      ctx.fillRect(25,25,100,100);
-      ctx.clearRect(45,45,60,60);
-      ctx.strokeRect(50,50,50,50);
-    }
+    var ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
+    ctx.strokeRect(0, 0, 200, 200);
   }
 });
