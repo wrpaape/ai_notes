2b445fbec420a95721fec5c01b6de69c5dfe85ed
Reid Paape <wrpaape@gmail.com>
2015-09-07 20:24:19 -0500

    experimenting with getters/setters

--- draw_app/app/assets/javascripts/components/index.js.jsx
+++ draw_app/app/assets/javascripts/components/index.js.jsx
@@ -5,25 +5,33 @@ var Index = React.createClass({
   componentDidMount: function() {
     var canvas = document.getElementsByTagName('canvas')[0];
     var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+    var pad = height / 10;
+    var carHeight = pad;
     var ar = 1 / 3;
-    var height = 150;
-    var base = height * ar;
-    var le = Math.sqrt(Math.pow(base / 2, 2) + Math.pow(height, 2));
-    var alpha = Math.atan((base / 2) / height);
+    var base = carHeight * ar;
+    var le = Math.sqrt(Math.pow(base / 2, 2) + Math.pow(carHeight, 2));
+    var alpha = Math.atan((base / 2) / carHeight);
+    var x0 = width / 2;
+    var y0 = height / 2;
+    var numDots = 10;
 
     this.setState({
-      canvas: canvas,
       ctx: ctx,
+      width: width,
+      height: height,
+      pad: pad,
       car : {
         base: base,
-        height: height,
+        height: carHeight,
         le: le,
         alpha: alpha,
-        x: 500,
-        y: 250,
+        x: x0,
+        y: y0,
         vx: 3,
         vy: 3,
-        color: 'blue',
+        color: 'pink',
         draw: function() {
           var x = this.x;
           var y = this.y;
@@ -38,10 +46,6 @@ var Index = React.createClass({
             aoa += Math.PI;
           }
 
-          // console.log('X: ' + vxSign + ' Y: ' + vySign);
-          // console.log('cos: ' + Math.cos(aoa - alpha));
-          // console.log('sin: ' + Math.sin(aoa - alpha));
-          console.log(aoa);
           ctx.beginPath();
           ctx.moveTo(x, y);
           ctx.lineTo(x - le * Math.cos(aoa - alpha), y - le * Math.sin(aoa - alpha));
@@ -49,28 +53,95 @@ var Index = React.createClass({
           ctx.closePath();
           ctx.fillStyle = this.color;
           ctx.fill();
+        },
+        update: function() {
+          this.x += this.vx;
+          this.y += this.vy;
+          if (this.y + this.vy > height - this.height || this.y + this.vy < this.height) {
+            this.vy = -Math.random() * 2 * this.vy;
+          }
+          if (this.x + this.vx > width - this.height || this.x + this.vx < this.height) {
+            this.vx = -Math.random() * 2 * this.vx;
+          }
         }
-      }
+      },
+      dots: this.resetDots(ctx, width, height, pad, x0, y0, numDots)
     }, this.draw);
+  },
+  Dot: function() {
+    var ctx;
+    var width;
+    var height;
+    var pad;
+    var x0;
+    var y0;
+    this.radius = pad * 3 / 20 + Math.random() * pad / 10;
+    pad += this.radius;
+    this.x = pad + Math.random() * (width - 2 * pad);
+    this.y = pad + Math.random() * (height - 2 * pad);
+    this.color = 'blue';
+    this.getDistance = function(x, y) {
+      return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)) - this.radius;
+    };
+    this.draw = function() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true);
+      ctx.closePath();
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    };
+  },
+  resetDots: function(numDots) {
+    var dots = [];
+    while (dots.length < numDots) {
+      dots.push(new this.Dot());
+    }
+
+    dots = {
+      dots: dots,
+      drawAll: function() {
+        this.dots.forEach(function(dot) {
+          dot.draw();
+        });
+      },
+      updateAll: function(x, y) {
+        var dots = this.dots.sort(function(a, b) {
+          [a, b].forEach(function(dot) {
+            dot.color = 'blue';
+            dot.distance = dot.getDistance(x, y);
+          });
+
+          return a.distance - b.distance;
+        });
+
+        var i = 0;
+        while (dots[i].distance < 0) {
+          dots[i++].reset();
+        }
+        dots[i].color = 'green';
+      }
+    };
+    dots.updateAll(x0, y0);
+
+    return dots;
   },
   render: function() {
     return <canvas width='1000' height='500' />;
   },
   draw: function() {
-    var canvas = this.state.canvas;
     var ctx = this.state.ctx;
+    var width = this.state.width;
+    var height = this.state.height;
+    var pad = this.state.pad;
     var car = this.state.car;
+    var dots = this.state.dots;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, width, height);
+    ctx.strokeRect(pad, pad, width - 2 * pad, height - 2 * pad);
     car.draw();
-    car.x += car.vx;
-    car.y += car.vy;
-    if (car.y + car.vy > canvas.height || car.y + car.vy < 0) {
-      car.vy = -car.vy;
-    }
-    if (car.x + car.vx > canvas.width || car.x + car.vx < 0) {
-      car.vx = -car.vx;
-    }
+    dots.drawAll();
+    car.update();
+    dots.updateAll(car.x, car.y);
     window.requestAnimationFrame(this.draw);
   }
 });
